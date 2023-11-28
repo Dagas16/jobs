4dbc79f0f5b1fd0af203126d84b10773bec2855e
Dag Åsmund Spångberg <dagas16@uia.no>
2023-11-28 23:19:11 +0100

    #12 added job applications

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="flex">
+    <div class="flex mb-10">
         <div class="grow">
             <h1 class="text-3xl font-extrabold">{{ $job['title'] }}</h1>
             <div>
@@ -14,5 +14,24 @@
                 <h2 class="text-xl text-bold">{{ $job['companyName'] }}</h2>
             </div>
         </div>
+    </div>
+    <div class="card bg-base-200 shadow-xl p-6">
+        <div class="card-body">
+            <h2 class="card-title text-2xl font-bold mb-3">
+                Your application
+            </h2>
+            <form action="/job/send-application/{{ $job['id'] }}" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <textarea class="textarea textarea-bordered w-full" name="cover_letter" rows="10"></textarea>
+
+                </div>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Send</button>
+                </div>
+            </form>
+
+        </div>
+
     </div>
 </x-layout>
