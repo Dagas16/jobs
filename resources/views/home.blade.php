faefd113aa3f60b8ab9fdd8ace4a0bca2aa6ff98
Dag Åsmund Spångberg <dagaasmund1@gmail.com>
2023-11-29 19:11:03 +0100

    Home page design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-3 gap-4">
         @foreach ($jobs as $job)
             <a href="/job/{{ $job['id'] }}">
-                <div class="card bg-base-200 hover:-translate-y-1 transition-all hover:shadow-md cursor-pointer">
+                <div class="card bg-base-200 hover:-translate-y-1 transition-all hover:shadow-md cursor-pointer h-full">
                     <div class="card-body">
                         <h2 class="card-title font-extrabold">{{ $job['title'] }}</h2>
                         <h3>{{ $job['companyName'] }}</h3>
